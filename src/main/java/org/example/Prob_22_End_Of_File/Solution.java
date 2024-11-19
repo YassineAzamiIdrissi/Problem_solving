55b1780a15a71ddede7f260b3b54dc31ba9da588
yassine <yassine@mail.com>
2024-11-19 10:30:07 +0100

    probs_22_23

---
+++ src/main/java/org/example/Prob_22_End_Of_File/Solution.java
@@ -0,0 +1,19 @@
+package org.example.Prob_22_End_Of_File;
+
+import java.util.Scanner;
+
+public class Solution {
+    static {
+        System.out.println("Hello class");
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = 0;
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            ++num;
+            System.out.println(num + " " + line);
+            if (line.endsWith(".")) break;
+        }
+    }
+}
---
+++ src/main/java/org/example/Prob_23_static_vars/Solution.java
@@ -0,0 +1,28 @@
+package org.example.Prob_23_static_vars;
+
+import java.util.Scanner;
+
+public class Solution {
+    static int B, H;
+
+    static {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            B = scanner.nextInt();
+            scanner.nextLine();
+            H = scanner.nextInt();
+            scanner.nextLine();
+            if (B <= 0 || H <= 0) {
+                throw new Exception();
+            }
+        } catch (Exception exp) {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+
+    public static void main(String[] args) {
+        if (B * H > 0 && H > 0) {
+            System.out.println(B * H);
+        }
+    }
+}
