982b5efa552e7871b61cfd037398ba30609aab9d
yassine <yassine@mail.com>
2024-11-20 08:47:00 +0100

    prob_29

---
+++ src/main/java/org/example/Prob_29_substrings/Solution.java
@@ -0,0 +1,13 @@
+package org.example.Prob_29_substrings;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start, end));
+    }
+}
