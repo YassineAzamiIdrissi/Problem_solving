a5c3d698b629fff96859c178377521602a4f8758
yassine <yassine@mail.com>
2024-11-22 00:19:05 +0100

    probs_36_37

---
+++ src/main/java/org/example/Prob_36_Tag_Based_Languages/Solution.java
@@ -0,0 +1,20 @@
+package org.example.Prob_36_Tag_Based_Languages;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        String regex = "^(<*>*</*>)$";
+        while (testCases > 0) {
+            String line = in.nextLine();
+            String[] parts = line.split(regex);
+            for (int i = 0; i < parts.length; i++) {
+                System.out.println(parts[i]);
+            }
+            testCases--;
+        }
+    }
+}
