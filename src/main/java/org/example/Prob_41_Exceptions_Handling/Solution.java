f8e7ee1ab5dfa81c4469b79e288574283783bfb2
yassine <yassine@mail.com>
2024-11-23 21:27:40 +0100

    prob_41_sketches

--- src/main/java/org/example/Prob_41_Exceptions_Handling/Solution.java
+++ src/main/java/org/example/Prob_41_Exceptions_Handling/Solution.java
@@ -5,15 +5,22 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String temp = "";
         try {
-            int num_1 = scanner.nextInt();
-            int num_2 = scanner.nextInt();
-            double division = num_1 / num_2;
+            Integer num_1 = scanner.nextInt();
+            // temp = num_1.toString();
+            Integer num_2 = scanner.nextInt();
+            int division = num_1 / num_2;
+            System.out.println(division);
         } catch (Exception exp) {
-            if (exp.getMessage() != null)
-                System.out.println(exp.getClass().toString().split(" ")[1] + ": " + exp.getMessage());
-            else
-                System.out.println(exp.getClass());
+            if (exp.getMessage() != null) {
+                if (exp instanceof ArithmeticException) {
+                    System.out.println(exp.getClass().toString().split(" ")[1] + ": " +
+                            exp.getMessage());
+                } else
+                    System.out.println(exp.getClass().toString().split(" ")[1]);
+            } else
+                System.out.println(exp.getClass().toString().split(" ")[1]);
         }
     }
 }
