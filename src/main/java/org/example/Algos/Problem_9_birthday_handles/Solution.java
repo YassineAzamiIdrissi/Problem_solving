e6ed6339064c4128461cb841f233d1f7b3da1279
yassine <yassine@mail.com>
2024-11-27 00:46:01 +0100

    algos_8_9

---
+++ src/main/java/org/example/Algos/Problem_9_birthday_handles/Solution.java
@@ -0,0 +1,42 @@
+package org.example.Algos.Problem_9_birthday_handles;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Solution {
+    public static int birthdayCakeCandles(List<Integer> candles) {
+        int maximum = 0, count = 0;
+        for (int i = 0; i < candles.size(); i++) {
+            if (candles.get(i) >= candles.get(maximum)) {
+                maximum = i;
+            }
+        }
+        for (int element : candles) {
+            if (element == candles.get(maximum)) {
+                ++count;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        int result = birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
