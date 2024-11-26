f6daa7285428fc9c66440919acf046515e1cc8b4
yassine <yassine@mail.com>
2024-11-26 17:16:27 +0100

    algos_7

---
+++ src/main/java/org/example/Algos/Problem_7_tree/Solution.java
@@ -0,0 +1,29 @@
+package org.example.Algos.Problem_7_tree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void staircase(int n) {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n - i; ++j) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i; ++k) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        staircase(n);
+
+        bufferedReader.close();
+    }
+}
